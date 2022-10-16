d28706e5308882c6e205ee5fbc6eab6622604285
KapilKumar7 <mrkapilsiwach@outlook.com>
2022-10-16 16:20:55 +0530

    Basic Structure of Project

--- CromeExtensions/Ideas.js
+++ CromeExtensions/Ideas.js
@@ -1,11 +1,12 @@
 const ideas = [
   {
-    Name: "Meditation-Minute", //if applicable
-    Type: "Mobile",
-    Description:
-      "This App will ring(a gong) on a specific interval of time ,like 1 minute or 3minute or any other time interval till that time meditator have to try to focus his/her mind on the meditation object like breath or any other meditation object",
-    Industry: "Wellness",
+    Name: "", //if applicable
+    Type: "",
+    Description:"",//As detailed as possible
+    Industry: "",
     Video: "", //if applicable
-    MatrixBreaker: "true", //Here Matrix breaker means if the app will help the user to came out negative Habit patterns Like- addiction, Hatered,lust ,cling to materialism and Take him/her to better control of mind ,Innerpeace ,More awareness ,Minimilism, Warriorness ,Self realisation ,Patience and love etc.
+    Link:"",   //if applicable
+    TeckStack:[],//if applicable    
+    MatrixBreaker: "", //Here Matrix breaker means if the app will help the user to came out negative Habit patterns Like- addiction, Hatered,lust ,cling to materialism and Take him/her to better control of mind ,Innerpeace ,More awareness ,Minimilism, Warriorness ,Self realisation ,Patience and love etc.
   },
 ];
--- MobileApps/Ideas.js
+++ MobileApps/Ideas.js
@@ -3,9 +3,10 @@ const ideas = [
     Name: "Meditation-Minute", //if applicable
     Type: "Mobile",
     Description:
-      "This App will ring(a gong) on a specific interval of time ,like 1 minute or 3minute or any other time interval till that time meditator have to try to focus his/her mind on the meditation object like breath or any other meditation object",
+      "This App will ring(a gong) on a specific interval of time ,like 1 minute or 3minute or any other time interval till that time meditator have to try to focus his/her mind on the meditation object like breath or any other meditation object",//As detailed as possible
     Industry: "Wellness",
     Video: "", //if applicable
+    Link:"",   //if applicable
     MatrixBreaker: "true", //Here Matrix breaker means if the app will help the user to came out negative Habit patterns Like- addiction, Hatered,lust ,cling to materialism and Take him/her to better control of mind ,Innerpeace ,More awareness ,Minimilism, Warriorness ,Self realisation ,Patience and love etc.
   },
 ];
