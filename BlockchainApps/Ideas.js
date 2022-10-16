69b91c4add28160dfd0108cc2b09c12f8249fd7c
Nikita0509 <74171229+Nikita0509@users.noreply.github.com>
2022-10-16 23:48:58 +0530

    Updated ideas.js

--- BlockchainApps/Ideas.js
+++ BlockchainApps/Ideas.js
@@ -1,12 +1,12 @@
 const ideas = [
   {
-    Name: "", //if applicable
-    Type: "",
-    Description:"",//As detailed as possible
-    Industry: "",
+    Name: "Ice-Box", //if applicable
+    Type: "Mobile",
+    Description:"This app will constantly track your pules rating and if your stressed it the app will suggest you different ideas to calm yourself down or suggest you some activites to do and control your stress.",//As detailed as possible
+    Industry: "Wellness",
     Video: "", //if applicable
     Link:"",   //if applicable
     TeckStack:[],//if applicable    
-    MatrixBreaker: "", //Here Matrix breaker means if the app will help the user to came out negative Habit patterns Like- addiction, Hatered,lust ,cling to materialism and Take him/her to better control of mind ,Innerpeace ,More awareness ,Minimilism, Warriorness ,Self realisation ,Patience and love etc.
+    MatrixBreaker: "true", //Here Matrix breaker means if the app will help the user to came out negative Habit patterns Like- addiction, Hatered,lust ,cling to materialism and Take him/her to better control of mind ,Innerpeace ,More awareness ,Minimilism, Warriorness ,Self realisation ,Patience and love etc.
   },
 ];
