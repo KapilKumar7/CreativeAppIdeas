fbf9804161efa74c170ca2a6385693fe049f2689
Kapil Kumar <100344844+KapilKumar7@users.noreply.github.com>
2022-10-17 14:53:02 +0530

    Added Ice-BoxApp Idea

--- MobileApps/Ideas.js
+++ MobileApps/Ideas.js
@@ -9,4 +9,14 @@ const ideas = [
     Link:"",   //if applicable
     MatrixBreaker: "true", //Here Matrix breaker means if the app will help the user to came out negative Habit patterns Like- addiction, Hatered,lust ,cling to materialism and Take him/her to better control of mind ,Innerpeace ,More awareness ,Minimilism, Warriorness ,Self realisation ,Patience and love etc.
   },
+   {
+    Name: "Ice-Box", //if applicable
+    Type: "Mobile",
+    Description:"This app will constantly track your pulse rating and if your stressed it the app will suggest you different ideas to calm yourself down or suggest you some activites to do and control your stress.",//As detailed as possible
+    Industry: "Wellness",
+    Video: "", //if applicable
+    Link:"",   //if applicable
+    TeckStack:[],//if applicable    
+    MatrixBreaker: "true", //Here Matrix breaker means if the app will help the user to came out negative Habit patterns Like- addiction, Hatered,lust ,cling to materialism and Take him/her to better control of mind ,Innerpeace ,More awareness ,Minimilism, Warriorness ,Self realisation ,Patience and love etc.
+  }
 ];
