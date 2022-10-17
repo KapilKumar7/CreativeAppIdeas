17b83b19f3f460d594523586af506809465f6857
sushrutlekurwale <83647918+sushrutlekurwale@users.noreply.github.com>
2022-10-17 23:28:43 +0530

    Added food ordering app idea

--- MobileApps/Ideas.js
+++ MobileApps/Ideas.js
@@ -19,4 +19,14 @@ const ideas = [
     TeckStack:[],//if applicable    
     MatrixBreaker: "true", //Here Matrix breaker means if the app will help the user to came out negative Habit patterns Like- addiction, Hatered,lust ,cling to materialism and Take him/her to better control of mind ,Innerpeace ,More awareness ,Minimilism, Warriorness ,Self realisation ,Patience and love etc.
   }
+  {
+    Name: "Food-cart", //if applicable
+    Type: "Mobile",
+    Description:
+      "This App will deliver the food ordered by you to your place as soon as possible",
+    Industry: "Online food ordering",
+    Video: "https://www.youtube.com/watch?v=9nWcPPHBzMk", //Video for reference only
+    MatrixBreaker: "false", //Here Matrix breaker means if the app will help the user to came out negative Habit patterns Like- addiction, Hatered,lust ,cling to materialism and Take him/her to better control of mind ,Innerpeace ,More awareness ,Minimilism, Warriorness ,Self realisation ,Patience and love etc.
+  }
+  
 ];
