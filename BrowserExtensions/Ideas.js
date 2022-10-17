24714cc20c0c8e5c226a427f44ba01707cae0f13
scott <scottajevans@gmail.com>
2022-10-18 00:21:48 +0100

    Amend the industry

--- BrowserExtensions/Ideas.js
+++ BrowserExtensions/Ideas.js
@@ -3,7 +3,7 @@ const ideas = [
     Name: "Bad Habit Blocker", //if applicable
     Type: "Browser extension",
     Description:"The extension will auto block unproductive websites if switched on and the user has not been productive recently. The user can configure a few things: days/times when the extension is in 'active' mode, how long the user must be productive for before they are able to regain access to social sites, a personal list of unproductive websites (for example youtube, reddit, facebook). The extension will register the currently focused site, and how long the focus has been held. If the user is deemed proactive, the sites will become unblocked and the user is free to procrastinate again if they wish.",//As detailed as possible
-    Industry: "Habit forming/Wellness",
+    Industry: "Habit/Wellness/Productivity",
     Video: "", //if applicable
     Link:"", //if applicable
     TeckStack:['JavaScript'], //if applicable
