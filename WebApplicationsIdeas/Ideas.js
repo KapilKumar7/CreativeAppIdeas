710120b31f326f515040374803db845e4e642c03
R C N <plbwymw@gmail.com>
2022-10-17 18:44:15 +0300

    tictactoe

--- WebApplicationsIdeas/Ideas.js
+++ WebApplicationsIdeas/Ideas.js
@@ -1,12 +1,12 @@
 const ideas = [
   {
-    Name: "", //if applicable
-    Type: "",
-    Description:"",//As detailed as possible
-    Industry: "",
+    Name: "Tic-Tac-Toe", //if applicable
+    Type: "WebApplicationsIdeas",
+    Description:"Graphic tic tac toe game",//As detailed as possible
+    Industry: "Full-Stack development",
     Video: "", //if applicable
-    Link:"",   //if applicable
-    TeckStack:[],//if applicable    
-    MatrixBreaker: "", //Here Matrix breaker means if the app will help the user to came out negative Habit patterns Like- addiction, Hatered,lust ,cling to materialism and Take him/her to better control of mind ,Innerpeace ,More awareness ,Minimilism, Warriorness ,Self realisation ,Patience and love etc.
+    Link:"https://www.youtube.com/watch?v=al_AgC2NSCo",   //if applicable
+    TeckStack:["HTML", "CSS", "Javascript"],//if applicable    
+    MatrixBreaker: "true", //Here Matrix breaker means if the app will help the user to came out negative Habit patterns Like- addiction, Hatered,lust ,cling to materialism and Take him/her to better control of mind ,Innerpeace ,More awareness ,Minimilism, Warriorness ,Self realisation ,Patience and love etc.
   },
 ];
