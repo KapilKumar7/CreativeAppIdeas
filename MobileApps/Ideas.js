e93f74e176af3da7247710e108c86dfee0e541ac
Kapil Kumar <100344844+KapilKumar7@users.noreply.github.com>
2022-10-18 19:44:57 +0530

    Removed unnecessary comments .

--- MobileApps/Ideas.js
+++ MobileApps/Ideas.js
@@ -10,23 +10,23 @@ const ideas = [
     MatrixBreaker: "true", //Here Matrix breaker means if the app will help the user to came out negative Habit patterns Like- addiction, Hatered,lust ,cling to materialism and Take him/her to better control of mind ,Innerpeace ,More awareness ,Minimilism, Warriorness ,Self realisation ,Patience and love etc.
   },
    {
-    Name: "Ice-Box", //if applicable
+    Name: "Ice-Box",
     Type: "Mobile",
-    Description:"This app will constantly track your pulse rating and if your stressed it the app will suggest you different ideas to calm yourself down or suggest you some activites to do and control your stress.",//As detailed as possible
+    Description:"This app will constantly track your pulse rating and if your stressed it the app will suggest you different ideas to calm yourself down or suggest you some activites to do and control your stress.",
     Industry: "Wellness",
-    Video: "", //if applicable
-    Link:"",   //if applicable
-    TeckStack:[],//if applicable    
-    MatrixBreaker: "true", //Here Matrix breaker means if the app will help the user to came out negative Habit patterns Like- addiction, Hatered,lust ,cling to materialism and Take him/her to better control of mind ,Innerpeace ,More awareness ,Minimilism, Warriorness ,Self realisation ,Patience and love etc.
+    Video: "",
+    Link:"",   
+    TeckStack:[], 
+    MatrixBreaker: "true", 
   },
   {
-    Name: "I am - Daily affirmations", //if applicable
+    Name: "I am - Daily affirmations", 
     Type: "Mobile",
-    Description:"This app will provide daily affirmations so that the user stays motivated and confident throughout the day. The user can also customise the affirmations shown.",//As detailed as possible
+    Description:"This app will provide daily affirmations so that the user stays motivated and confident throughout the day. The user can also customise the affirmations shown.",
     Industry: "Wellness and mental health",
-    Video: "", //if applicable
-    Link:"",   //if applicable
-    TeckStack:[],//if applicable    
-    MatrixBreaker: "true", //Here Matrix breaker means if the app will help the user to came out negative Habit patterns Like- addiction, Hatered,lust ,cling to materialism and Take him/her to better control of mind ,Innerpeace ,More awareness ,Minimilism, Warriorness ,Self realisation ,Patience and love etc.
+    Video: "", 
+    Link:"",   
+    TeckStack:[],   
+    MatrixBreaker: "true",
   }
 ];
