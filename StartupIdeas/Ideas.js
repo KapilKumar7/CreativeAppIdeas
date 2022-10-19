1ea8700f82bc40d344f6e26ae1e8232929a2764a
Kenee Patel <59523692+KeneePatel@users.noreply.github.com>
2022-10-19 15:06:18 +0530

    added Intern Helper Idea

--- StartupIdeas/Ideas.js
+++ StartupIdeas/Ideas.js
@@ -9,4 +9,10 @@ const ideas = [
     TeckStack:[],//if applicable    
     MatrixBreaker: "", //Here Matrix breaker means if the app will help the user to came out negative Habit patterns Like- addiction, Hatered,lust ,cling to materialism and Take him/her to better control of mind ,Innerpeace ,More awareness ,Minimilism, Warriorness ,Self realisation ,Patience and love etc.
   },
+  {
+    Name: "Intern Helper",
+    Type: "Business",
+    Description: "An organization which helps other organizations act as a mediator between finding potential candidates for internship. Helps both people finding internship opportunities and also organizations in finding worthy interns. So, a win-win for both. The business aspect of this idea would be to get some percentage out of the stipend provided from organization or intern and hence the funcding can be then used to further increase the quality of the mediator organization.",
+    MatrixBreaker: "true",
+  },
 ];
