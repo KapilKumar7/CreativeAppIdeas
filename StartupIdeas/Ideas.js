8733079aa036227929bc394da9fe330ba86366d2
Wangata <martinwangata@gmail.com>
2022-10-20 16:27:38 +0300

    New startup idea

--- StartupIdeas/Ideas.js
+++ StartupIdeas/Ideas.js
@@ -6,13 +6,20 @@ const ideas = [
     Industry: "",
     Video: "", //if applicable
     Link:"",   //if applicable
-    TeckStack:[],//if applicable    
+    TechStack:[],//if applicable    
     MatrixBreaker: "", //Here Matrix breaker means if the app will help the user to came out negative Habit patterns Like- addiction, Hatered,lust ,cling to materialism and Take him/her to better control of mind ,Innerpeace ,More awareness ,Minimilism, Warriorness ,Self realisation ,Patience and love etc.
   },
   {
     Name: "Intern Helper",
     Type: "Business",
-    Description: "An organization which helps other organizations act as a mediator between finding potential candidates for internship. Helps both people finding internship opportunities and also organizations in finding worthy interns. So, a win-win for both. The business aspect of this idea would be to get some percentage out of the stipend provided from organization or intern and hence the funcding can be then used to further increase the quality of the mediator organization.",
+    Description: "An organization which helps other organizations act as a mediator between finding potential candidates for internship. Helps both people finding internship opportunities and also organizations in finding worthy interns. So, a win-win for both. The business aspect of this idea would be to get some percentage out of the stipend provided from organization or intern and hence the funding can be then used to further increase the quality of the mediator organization.",
     MatrixBreaker: "true",
+  },
+  {
+    Name: "CardEqual", 
+    Type: "Business",
+    Description:"Being able to diagnose and treat heart disease is a huge challenge. CardEqual is a platform that allows patients to share their medical data with researchers and doctors. This data is then used to create a model that can predict the risk of heart disease. The model is then used to create a personalized treatment plan for the patient. The platform is also used to create a community of patients and doctors to share their experiences and help each other.",
+    Industry: "Healthcare",   
+    TechStack:[],    
   },
 ];
