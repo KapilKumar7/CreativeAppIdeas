9bfcc01f11fc68bd9975fe6f0b0e9e374b1d0b56
Emaad Ahmed <emaada21@iitk.ac.in>
2022-10-21 22:06:42 +0530

    Added ideas in MobileApps and Browser Extension

--- MobileApps/Ideas.js
+++ MobileApps/Ideas.js
@@ -104,6 +104,13 @@ const ideas = [
     Description: "This app lets you put in a property address and see a list of food delivery services that can reach the address. It will show a map of the area and the 'cut off' points of other deliver services to help buyers/renters choose a desired property to rent,",
     Industry: "Real Estate",
     MatrixBreaker: "false"
+  },
+    {
+    Name: "Taskovania",
+    Type: "Mobile",
+    Description: "This app will basically be a game-cum-to-do list app, where the user will setup some tasks he/she has to complete in a day and the app will feature either a medieval creature like dragon etc or an medival rpg character like knight or mage etc (which user can choose). The more tasks the user completes in a day the more points he/she can earn as a reward,the faster they complete the task more the reward, these points can then be traded for upgrading the character. This app can also be developed in such a way that different people can then use their characters to compete in pvp battles. So basically the more productive a person is in a day the stronger his/her character will be which in turn will boost their ranking say on a leaderboard",
+    Industry: "Productivity",
+    MatrixBreaker: "false"
   }
 
 ];
